21548af44c415f0e07fc09a93e2b66a3edf642c5
x0jac0b0x <jacobnowak15@gmail.com>
2026-06-06 17:14:50 -0600

    Update white-rabbit.ino

--- white-rabbit.ino
+++ white-rabbit.ino
@@ -30,12 +30,20 @@ constexpr int CSN1_PIN = 17;
 constexpr int CE2_PIN  = 11;
 constexpr int CSN2_PIN = 16;
 
+#define BOOT_BUTTON 9
+
+bool wifiMode = false;  // false = Bluetooth/BLE, true = WiFi
+bool lastButtonState = HIGH;
+unsigned long lastPressTime = 0;
+const unsigned long debounceDelay = 250;
+
 SPIClass *spiFSPI = nullptr;
 
 RF24 radio1(CE1_PIN, CSN1_PIN, SPI_SPEED);
 RF24 radio2(CE2_PIN, CSN2_PIN, SPI_SPEED);
 
 int bluetooth_channels[] = {32, 34, 46, 48, 50, 52, 0, 1, 2, 4, 6, 8, 22, 24, 26, 28, 30, 74, 76, 78, 80};
+int WiFi_channels[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
 #define RGB_LED_PIN 8
 #define NUM_LEDS 1
@@ -43,10 +51,34 @@ int bluetooth_channels[] = {32, 34, 46, 48, 50, 52, 0, 1, 2, 4, 6, 8, 22, 24, 26
 Adafruit_NeoPixel rgb(NUM_LEDS, RGB_LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void configureRadio(RF24 &radio, int channel, SPIClass *spi);
+void test();
+void test1();
 
 void setup() {
     Serial.begin(115200);
     delay(1000);
+
+    pinMode(BOOT_BUTTON, INPUT_PULLUP);
+
+    rgb.begin();
+    rgb.setBrightness(50);
+    rgb.setPixelColor(0, rgb.Color(255, 255, 255));  // waiting = white
+    rgb.show();
+
+    while (digitalRead(BOOT_BUTTON) == HIGH) {
+        delay(10);
+    }
+
+    while (digitalRead(BOOT_BUTTON) == LOW) {
+        delay(10);
+    }
+
+    wifiMode = false;
+    lastButtonState = HIGH;
+    lastPressTime = millis();
+
+    rgb.setPixelColor(0, rgb.Color(0, 0, 255));  // Bluetooth/BLE = blue
+    rgb.show();
 
     esp_bt_controller_deinit();
     esp_wifi_stop();
@@ -58,11 +90,6 @@ void setup() {
 
     configureRadio(radio1, bluetooth_channels[0], spiFSPI);
     configureRadio(radio2, bluetooth_channels[0], spiFSPI);
-
-    rgb.begin();
-    rgb.setBrightness(50);
-    rgb.setPixelColor(0, rgb.Color(255, 255, 255));
-    rgb.show();
 }
 
 void configureRadio(RF24 &radio, int channel, SPIClass *spi) {
@@ -78,6 +105,41 @@ void configureRadio(RF24 &radio, int channel, SPIClass *spi) {
 }
 
 void loop() {
+    bool buttonState = digitalRead(BOOT_BUTTON);
+
+    if (buttonState == LOW && lastButtonState == HIGH) {
+        if (millis() - lastPressTime > debounceDelay) {
+            wifiMode = !wifiMode;
+
+            if (wifiMode) {
+                rgb.setPixelColor(0, rgb.Color(255, 0, 0));  // WiFi = red
+            } else {
+                rgb.setPixelColor(0, rgb.Color(0, 0, 255));  // Bluetooth/BLE = blue
+            }
+
+            rgb.show();
+            lastPressTime = millis();
+        }
+    }
+
+    lastButtonState = buttonState;
+
+    if (wifiMode) {
+        jamWiFi();
+    } else {
+        jamBluetooth();
+    }
+}
+
+void jamWiFi() {
+    int randomIndex = random(0, sizeof(WiFi_channels) / sizeof(WiFi_channels[0]));
+    int channel = WiFi_channels[randomIndex];
+
+    radio1.setChannel(channel);
+    radio2.setChannel(channel);
+}
+
+void jamBluetooth() {
     int randomIndex = random(0, sizeof(bluetooth_channels) / sizeof(bluetooth_channels[0]));
     int channel = bluetooth_channels[randomIndex];
 
