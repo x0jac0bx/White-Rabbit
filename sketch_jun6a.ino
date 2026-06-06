e26750ccbe1d78ee62403420f0e00336ec9c94f5
x0jac0b0x <jacobnowak15@gmail.com>
2026-06-06 14:22:31 -0600

    Add files via upload

---
+++ sketch_jun6a.ino
@@ -0,0 +1,77 @@
+/*
+  Project: WhiteRabbit Bluetooth Jammer
+  Creator: x0jacob0x
+
+  Special thanks to the RF-Clown project
+  by cifertech:
+  https://github.com/cifertech/RF-Clown
+
+  Inspired by RF-Clown for educational RF research.
+*/
+
+#include "RF24.h"
+#include <SPI.h>
+#include "esp_bt.h"
+#include "esp_wifi.h"
+
+constexpr int SPI_SPEED = 8000000;
+
+// Shared SPI bus
+constexpr int SCK_PIN  = 6;
+constexpr int MOSI_PIN = 7;
+constexpr int MISO_PIN = 2;
+
+// NRF-1 (LEFT)
+constexpr int CE1_PIN  = 10;
+constexpr int CSN1_PIN = 17;
+
+// NRF-2 (RIGHT)
+constexpr int CE2_PIN  = 11;
+constexpr int CSN2_PIN = 16;
+
+SPIClass *spiFSPI = nullptr;
+
+RF24 radio1(CE1_PIN, CSN1_PIN, SPI_SPEED);
+RF24 radio2(CE2_PIN, CSN2_PIN, SPI_SPEED);
+
+int bluetooth_channels[] = {32, 34, 46, 48, 50, 52, 0, 1, 2, 4, 6, 8, 22, 24, 26, 28, 30, 74, 76, 78, 80};
+
+void configureRadio(RF24 &radio, int channel, SPIClass *spi);
+
+void jamBluetooth();
+
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+
+    esp_bt_controller_deinit();
+    esp_wifi_stop();
+    esp_wifi_deinit();
+    esp_wifi_disconnect();
+
+    spiFSPI = new SPIClass(FSPI);
+    spiFSPI->begin(SCK_PIN, MISO_PIN, MOSI_PIN);
+
+    configureRadio(radio1, bluetooth_channels[0], spiFSPI);
+    configureRadio(radio2, bluetooth_channels[0], spiFSPI);
+}
+
+void configureRadio(RF24 &radio, int channel, SPIClass *spi) {
+    if (radio.begin(spi)) {
+        radio.setAutoAck(false);
+        radio.stopListening();
+        radio.setRetries(0, 0);
+        radio.setPALevel(RF24_PA_MAX, true);
+        radio.setDataRate(RF24_2MBPS);
+        radio.setCRCLength(RF24_CRC_DISABLED);
+        radio.startConstCarrier(RF24_PA_HIGH, channel);
+    }
+}
+
+void loop() {
+    int randomIndex = random(0, sizeof(bluetooth_channels) / sizeof(bluetooth_channels[0]));
+    int channel = bluetooth_channels[randomIndex];
+
+    radio1.setChannel(channel);
+    radio2.setChannel(channel);
+}
